ef1469d3092f0390958272beb0f183169bc19622
FreshOceans <mperezz398@gmail.com>
2017-06-09 14:43:55 -0400

    fixed api issue, able to search track

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-// Your SoundCloud enabled jukebox should allow the user to do the following things:
-//     • Play a track off of SoundCloud based on its track ID
-//     • Pause the currently playing track
-//     • Display the following current track information:
-//         Artist name with link to his/her profile page
-//         Title with link to track's page
-//         Description and genre
-//         Artwork
-
 $(document).ready(function() {
-    console.log("jQuery loaded/document ready");
-    tracks:[],
+  console.log("jQuery ready");
 
-    // ====== initializing SoundCloud API ======
-    SC.initialize({
-      client_id: 'fd4e76fc67798bfa742089ed619084a6s'
-    });
-	console.log("SC:", SC);
+    var soundBox = {
+        clientId: 'fd4e76fc67798bfa742089ed619084a6',
+        musicLibrary: [
+        ],
+        // ====== Initialize App ======
+        initialize: function() {
+          var self = this;
+          SC.initialize({
+            client_id: self.clientId
+          });
+          console.log("SC:", SC);
+          this.getSearchBtn();
+        },
+        // ======== ACTIVATE SEARCH BUTTON =========
+        getSearchBtn: function() {
+            var self = this;
+            $('#searchBtn').on('click', function(){
+              console.log("-- click:search --");
+              self.getSongInfo();
+            });
+        },
+          // ======= SEARCH SONG AND DISPLAY ON CONSOLE ========
+        getSongInfo: function() {
+            var self = this;
+            var searchText = $('#search').val();
+            console.log("search:", searchText);
+            SC.get('/tracks', {
+              q: searchText
+                }).then(function(tracks){
+                  console.log("==SC.get:tracks==");
+                  console.log("tracks:", tracks);
+                  self.trackList = tracks;            // store in app
+                  self.displayTrackList(tracks);      // callback that displays track titles
+                  self.activateListItems();           //callback that activates title onlist
+                });
+        },
 
-    var searchItem = $('#searchText').val();
-    console.log("== searchItem ==");
-    // SC.get('/tracks', {
-    //   q: searchItem
-    // }).then(function(tracks) {
-    //   console.log("== SC.get:tracks ==");
-    //   console.log("tracks:", tracks);     // checking results
-    //   self.trackList = tracks;            // store in app
-    //   self.displayTrackList(tracks);      // callback that displays track titles
-    //   self.activateListItems();           // callback that activates titles on list
-    // });
-    //
 
-});
+    }; // CLOSE soundBox
+
+    soundBox.initialize();
+}); // CLOSE jQuery
