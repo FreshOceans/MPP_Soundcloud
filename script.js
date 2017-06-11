4603fff272f63c7c195492d53a76d34ac2698411
FreshOceans <mperezz398@gmail.com>
2017-06-11 12:51:38 -0400

    integrated pause play buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Play a track off of SoundCloud based on its track ID
+// Pause the currently playing track
+// Display the following current track information:
+//      Artist name with link to his/her profile page
+//      Title with link to track's page
+//      Description, genre and release date
+//      Artwork
+
 $(document).ready(function() {
   console.log("jQuery ready");
 
@@ -12,7 +20,8 @@ $(document).ready(function() {
             client_id: self.clientId
           });
           console.log("SC:", SC);
-          this.getSearchBtn();
+          self.getSearchBtn();
+          self.activateAudioButtons();
         },
         // ======== ACTIVATE SEARCH BUTTON =========
         getSearchBtn: function() {
@@ -34,8 +43,8 @@ $(document).ready(function() {
                   console.log("==SC.get:tracks==");
                   console.log("tracks:", tracks);
                   self.musicLibrary = tracks;
-                  self.displayTrackList(tracks);      // callback that displays track titles
-                  self.activateListItems();           //callback that activates title onlist
+                  self.displayTrackList(tracks);
+                  self.activateListItems();
                 });
         },
         // ======= DISPLAY SEARCH RESULTS ======
@@ -64,12 +73,31 @@ $(document).ready(function() {
         // ======= STREAM TRACK =======
         playSelectedSong: function(trackId) {
             console.log("== playSelectedSong ==");
+            // SC.oEmbed("/tracks/" + trackId, { auto_play: true }).then(function(oEmbed) {
+            //     console.log('oEmbed response: ', oEmbed);
+            // });
             SC.stream("/tracks/" + trackId).then(function(player) {
                 self.player = player;
                 player.play();
             });
-        }
+            // $('#artwork').scPlayer({
+            //     loadArtworks: 1
+            // });
+        },
+        activateAudioButtons: function(){
+            console.log("== activateAudioButtons ==");
+            var self = this;
+            var audioPlayer = $('#audioPlayer');
+            $('#playBtn').on('click', function(event){
+                console.log('-- playBtn --');
+                player.play();
+            });
+            $('#pauseBtn').on('click', function(event){
+                console.log('-- pauseBtn --');
+                player.pause();
+            });
 
+        }
 
 
 
